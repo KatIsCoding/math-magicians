c74c104d54f71b51b839a3767682b5797069dc03
KatIsCoding <fabrigoru@gmail.com>
2021-10-21 02:28:28 -0600

    Refactor App component from Function to Class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import React from 'react';
 import Calc from './components/Calculator';
 
-function App() {
-  return (
-    <div>
-      <Calc />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Calc />
+      </div>
+    );
+  }
 }
 
 export default App;
