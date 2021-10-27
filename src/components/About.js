a597a1a02107427ed24ba3aa2b2fcc03f03aa572
KatIsCoding <fabrigoru@gmail.com>
2021-10-26 19:59:17 -0600

    Routes to the different pages as well as their respective styles added

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <div className="about">
+      <h1>Welcome to our page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Quisquam quos quis doloremque, quae quam quisquam dolorum.
+        Quisquam, quisquam. Quisquam, quisquam.  Quisquam, quisquam.
+      </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <p>
+        Mathematics is not about numbers, equations, computations, or algorithms.
+        It is about understanding.
+        - William Paul Thurston
+      </p>
+    </div>
+  );
+}
+
+export default Home;
